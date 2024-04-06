fced3728a53019992cbca6e3290eaf8e6b3d1ac7
lee jeongmin <ljm991108@gmail.com>
2024-04-07 06:35:14 +0900

    baseUrl 교체

--- src/_common/utils/axios.ts
+++ src/_common/utils/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 import qs from 'qs'
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://43.201.108.75:8000/',
+  baseURL: 'https://peacetol.n-e.kr/',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
