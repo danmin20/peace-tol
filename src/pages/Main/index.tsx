c5c808d66e477b338f739a134cbe17c85208cca7
kang <kangju2000@naver.com>
2024-04-07 07:47:22 +0900

    안쓰는 import 제거

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { RecordListRow } from './components/RecordListRow'
 import {
