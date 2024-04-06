8dce5641b1211d94f18d392b3e12417d97d73761
Lee Jeong Min <danmin20@khu.ac.kr>
2024-04-06 13:55:22 +0900

    컬러셋, 공통 컴포넌트 추가

---
+++ src/_common/components/Ballon/style.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react'
+
+import { color } from '../../styles/color'
+
+export const BallonStyle = css({
+  padding: '36px 16px',
+  backgroundColor: color.white,
+  fontWeight: 500,
+  fontSize: '16px',
+  borderRadius: '0px 36px 36px 36px',
+  boxShadow: '5px 5px 30px 0px #61616133'
+})
